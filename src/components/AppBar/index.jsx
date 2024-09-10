d0a3385b7c0920c3fb1881a829416702d2f0e1f9
Dương Hà Tuấn <hatuan1423@gmail.com>
2024-09-11 00:19:08 +0700

    code header duoc co chut xiu

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,18 +1,45 @@
 import Box from "@mui/material/Box";
-import ModeSelect from "../../components/ModeSelect";
+import ModeSelect from "~/components/ModeSelect";
+import AppsIcon from "@mui/icons-material/Apps";
+import TrelloIcon from "~/assets/trello.svg";
+import { Typography } from "@mui/material";
+import WorkSpace from "./Menus/WorkSpace";
+import Recent from "./Menus/Recent";
+import Started from "./Menus/Started";
+import Templates from "./Menus/Templates";
 
 const AppBar = () => {
   return (
     <Box
+      px={2}
       sx={{
-        backgroundColor: "primary.light",
         width: "100%",
         height: (theme) => theme.trelloCustom.appBarHeight,
         display: "flex",
         alignItems: "center",
+        justifyContent: "space-between",
       }}
     >
-      <ModeSelect />
+      <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+        <AppsIcon sx={{ color: "primary.main" }} />
+        <img
+          src={TrelloIcon}
+          alt="trelloIcon"
+          style={{ width: "30px", height: "30px" }}
+        />
+        <Typography
+          sx={{ fontSize: "1.2rem", fontWeight: "bold", color: "primary.main" }}
+        >
+          Trello
+        </Typography>
+        <WorkSpace />
+        <Recent />
+        <Started />
+        <Templates />
+      </Box>
+      <Box>
+        <ModeSelect />
+      </Box>
     </Box>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { CssBaseline } from "@mui/material";
 import App from "./App.jsx";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme.js";
-import { CssBaseline } from "@mui/material";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
